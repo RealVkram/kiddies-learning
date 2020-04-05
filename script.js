32ff9e5bc9368f53d2167aef9479910b526be92f
RealVkram <ajayiadegbemi@gmail.com>
2020-04-05 21:34:22 +0100

    returned the sound on play

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const firstHandler = () => {
   const audioA = document.getElementById("audioLetterA");
-  audioA.play();
+  return audioA.play();
 };
 const letterA = document.getElementById("letterA");
-letterA.addEventListener("mouseover", firstHandler);
+letterA.addEventListener("click", firstHandler);
